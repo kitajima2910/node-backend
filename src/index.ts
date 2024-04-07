898edc082157a4e0c267b67dcc7957e703a7abd5
kitajima2910 <kitajima2910@gmail.com>
2024-04-07 15:42:58 +0700

    MongoDB Setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser'
 import cookieParser from 'cookie-parser'
 import compression from 'compression'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
 const app = express()
 
@@ -19,4 +20,12 @@ const server = http.createServer(app)
 
 server.listen(8080, () => {
     console.log('Server running on http://localhost:8080/');
+})
+
+const MONGO_URL = 'mongodb+srv://kitajima2910:qwer1234@cluster0.t1u6pv0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+
+mongoose.Promise = Promise
+mongoose.connect(MONGO_URL)
+mongoose.connection.on('error', (error: Error) => {
+    console.log(error)
 })
